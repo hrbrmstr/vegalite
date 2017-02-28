73fcda7f555b6eb50487219e0c4b17aaabf3ecbe
jsonbecker <jason@jbecker.co>
2017-02-27 20:43:19 -0800

    Field needed default value

--- inst/htmlwidgets/spec.js
+++ inst/htmlwidgets/spec.js
@@ -28,17 +28,17 @@ HTMLWidgets.widget({
     if (d3.select("#vis-tooltip").empty()) {
       d3.select("body").append("div")
         .attr("id", "vis-tooltip")
-        .attr("class", "vg-tooltip")
+        .attr("class", "vg-tooltip");
 
     } else {
       d3.select("#vis-tooltip").attr("class","vg-tooltip");
-    };
+    }
 
   vg.embed(el, embedSpec, function(error, result) {
     // Callback receiving the View instance and parsed Vega spec
     // result.view is the View, which resides under the '#vis' element
     // added by Jia for vg tooltip
-    vl.tooltip(result.view, JSON.parse(x.spec))
+    vl.tooltip(result.view, JSON.parse(x.spec));
   });
 
   },
