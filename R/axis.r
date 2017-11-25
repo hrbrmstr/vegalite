4d89a8bf0ebfea1c9775ae6230c8eeb03adb6bbe
AliciaSchep <aschep@gmail.com>
2017-11-24 17:47:39 -0800

    update axis functions for new schema

--- R/axis.r
+++ R/axis.r
@@ -15,24 +15,17 @@ vl <- wdgt_png_tf <- NULL
 #' @param vl Vega-Lite object
 #' @param chnl
 #'        x, y, column, or row
-#' @param axisColor,axisWidth,offset,orient,zindex
-#'        see \href{https://vega.github.io/vega-lite/docs/axis.html#general}{axis docs} &
-#'        \href{https://github.com/vega/vega-lite/blob/master/src/axis.ts}{axis base config}
-#' @param grid,gridColor,gridDash,gridOpacity,gridWidth
-#'        see \href{https://vega.github.io/vega-lite/docs/axis.html#grid}{axis docs} &
-#'        \href{https://github.com/vega/vega-lite/blob/master/src/axis.ts}{axis base config}
-#' @param labels,format,labelAngle,labelAlign,labelBaseline,labelMaxLength,shortTimeLabels
-#'        see \href{https://vega.github.io/vega-lite/docs/axis.html#labels}{axis docs} &
-#'        \href{https://github.com/vega/vega-lite/blob/master/src/axis.ts}{axis base config}
-#' @param subdivide,ticks,tickColor,tickLabelColor,tickLabelFont,tickLabelFontSize,tickPadding,tickSize,tickSizeMajor,tickSizeMinor,tickSizeEnd,tickWidth,values
-#'        see \href{https://vega.github.io/vega-lite/docs/axis.html#ticks}{axis docs} &
-#'        \href{https://github.com/vega/vega-lite/blob/master/src/axis.ts}{axis base config}
-#' @param title,titleColor,titleFont,titleFontWeight,titleFontSize,titleOffset,titleMaxLength,characterWidth
-#'        see \href{https://vega.github.io/vega-lite/docs/axis.html#title}{axis docs} &
-#'        \href{https://github.com/vega/vega-lite/blob/master/src/axis.ts}{axis base config}
+#' @param domain,grid,maxExtent,minExtent,orient,offset,position,zindex
+#'        see \href{https://vega.github.io/vega-lite/docs/axis.html#general}{axis docs}
+#' @param format,labels,labelAngle,labelOverlap,labelPadding
+#'        see \href{https://vega.github.io/vega-lite/docs/axis.html#labels}{axis docs}
+#' @param ticks,tickCount,tickSize,values
+#'        see \href{https://vega.github.io/vega-lite/docs/axis.html#ticks}{axis docs}
+#' @param title,titleMaxLength,titlePadding
+#'        see \href{https://vega.github.io/vega-lite/docs/axis.html#title}{axis docs}
 #' @param remove
-#'        see \href{http://vega.github.io/vega-lite/docs/axis.html}{axis docs} &
-#'        \href{https://github.com/vega/vega-lite/blob/master/src/axis.ts}{axis base config}
+#'        see \href{http://vega.github.io/vega-lite/docs/axis.html}{axis docs}
+#' @param ... deprecated arguments
 #' @encoding UTF-8
 #' @references \href{http://vega.github.io/vega-lite/docs/axis.html}{Vega-List Axis spec}
 #' @export
@@ -54,61 +47,53 @@ vl <- wdgt_png_tf <- NULL
 #'   mark_bar()
 #'
 
-axis_vl <- function(vl, chnl = "x", axisColor=NULL, axisWidth=NULL, offset=NULL,
-                    orient=NULL, zindex=NULL, grid=NULL, gridColor=NULL,
-                    gridDash=NULL, gridOpacity=NULL, gridWidth=NULL,
-                    labels=TRUE, format=NULL, labelAngle=NULL, labelAlign=NULL,
-                    labelBaseline=NULL, labelMaxLength=25, shortTimeLabels=NULL,
-                    subdivide=NULL, ticks=NULL, tickColor=NULL,
-                    tickLabelColor=NULL, tickLabelFont=NULL,
-                    tickLabelFontSize=NULL, tickPadding=NULL, tickSize=NULL,
-                    tickSizeMajor=NULL, tickSizeMinor=NULL, tickSizeEnd=NULL,
-                    tickWidth=NULL, values=NULL, title="", titleColor=NULL,
-                    titleFont=NULL, titleFontWeight=NULL, titleFontSize=NULL,
-                    titleOffset=NULL, titleMaxLength=NULL, characterWidth=6,
-                    remove=FALSE) {
+axis_vl <- function(vl, chnl = "x",
+                    domain=NULL,grid=NULL,maxExtent=NULL,minExtent=NULL,
+                    orient=NULL,offset=NULL,position=NULL,zindex=NULL,
+                    format=NULL,labels=NULL,labelAngle=NULL,labelOverlap=NULL,
+                    labelPadding=NULL,ticks=NULL,tickCount=NULL,tickSize=NULL,
+                    values=NULL,title=NULL,titleMaxLength=NULL,titlePadding=NULL,
+                    remove=FALSE, ...) {
+
+  if (length(list(...)) > 0) {
+    warning("Supplied arguments to axis_vl that are not supported by vegalite 2: ",
+            paste(names(list(...)),collapse = ","),". ",
+            "\nThose arguments will be ignored. See ?axis_vl for supported ",
+            "arguments.")
+  }
 
   if (remove) {
     vl$x$encoding[[chnl]]$axis <- NA
   } else {
-    if (!is.null(axisColor))          vl$x$encoding[[chnl]]$axis$axisColor <- axisColor
-    if (!is.null(zindex))             vl$x$encoding[[chnl]]$axis$zindex <- zindex
-    if (!is.null(axisWidth))          vl$x$encoding[[chnl]]$axis$axisWidth <- axisWidth
-    if (!is.null(offset))             vl$x$encoding[[chnl]]$axis$offset <- offset
+
+    # General
+    if (!is.null(domain))             vl$x$encoding[[chnl]]$axis$domain <- domain
     if (!is.null(grid))               vl$x$encoding[[chnl]]$axis$grid <- grid
-    if (!is.null(gridColor))          vl$x$encoding[[chnl]]$axis$gridColor <- gridColor
-    if (!is.null(gridDash))           vl$x$encoding[[chnl]]$axis$gridDash <- gridDash
-    if (!is.null(gridOpacity))        vl$x$encoding[[chnl]]$axis$gridOpacity <- gridOpacity
-    if (!is.null(gridWidth))          vl$x$encoding[[chnl]]$axis$gridWidth <- gridWidth
+    if (!is.null(maxExtent))          vl$x$encoding[[chnl]]$axis$maxExtent <- maxExtent
+    if (!is.null(minExtent))          vl$x$encoding[[chnl]]$axis$minExtent <- minExtent
+    if (!is.null(orient))             vl$x$encoding[[chnl]]$axis$orient <- orient
+    if (!is.null(offset))             vl$x$encoding[[chnl]]$axis$offset <- offset
+    if (!is.null(position))           vl$x$encoding[[chnl]]$axis$position <- position
+    if (!is.null(zindex))             vl$x$encoding[[chnl]]$axis$zindex <- zindex
+
+    #Labels
+    if (!is.null(format))             vl$x$encoding[[chnl]]$axis$format <- format
     if (!is.null(labels))             vl$x$encoding[[chnl]]$axis$labels <- labels
     if (!is.null(labelAngle))         vl$x$encoding[[chnl]]$axis$labelAngle <- labelAngle
-    if (!is.null(labelAlign))         vl$x$encoding[[chnl]]$axis$labelAlign <- labelAlign
-    if (!is.null(labelBaseline))      vl$x$encoding[[chnl]]$axis$labelBaseline <- labelBaseline
-    if (!is.null(labelMaxLength))     vl$x$encoding[[chnl]]$axis$labelMaxLength <- labelMaxLength
-    if (!is.null(shortTimeLabels))    vl$x$encoding[[chnl]]$axis$shortTimeLabels <- shortTimeLabels
-    if (!is.null(subdivide))          vl$x$encoding[[chnl]]$axis$subdivide <- subdivide
+    if (!is.null(labelOverlap))       vl$x$encoding[[chnl]]$axis$labelOverlap <- labelOverlap
+    if (!is.null(labelPadding))       vl$x$encoding[[chnl]]$axis$labelOverlap <- labelPadding
+
+    #Ticks
     if (!is.null(ticks))              vl$x$encoding[[chnl]]$axis$ticks <- ticks
-    if (!is.null(tickColor))          vl$x$encoding[[chnl]]$axis$tickColor <- tickColor
-    if (!is.null(tickLabelColor))     vl$x$encoding[[chnl]]$axis$tickLabelColor <- tickLabelColor
-    if (!is.null(tickLabelFont))      vl$x$encoding[[chnl]]$axis$tickLabelFont <- tickLabelFont
-    if (!is.null(tickLabelFontSize))  vl$x$encoding[[chnl]]$axis$tickColor <- tickLabelFontSize
-    if (!is.null(tickWidth))          vl$x$encoding[[chnl]]$axis$tickWidth <- tickWidth
-    if (!is.null(tickPadding))        vl$x$encoding[[chnl]]$axis$tickPadding <- tickPadding
+    if (!is.null(tickCount))          vl$x$encoding[[chnl]]$axis$tickCount <- tickCount
     if (!is.null(tickSize))           vl$x$encoding[[chnl]]$axis$tickSize <- tickSize
-    if (!is.null(tickSizeMajor))      vl$x$encoding[[chnl]]$axis$tickSizeMajor <- tickSizeMajor
-    if (!is.null(tickSizeMinor))      vl$x$encoding[[chnl]]$axis$tickSizeMinor <- tickSizeMinor
-    if (!is.null(tickSizeEnd))        vl$x$encoding[[chnl]]$axis$tickSizeEnd <- tickSizeEnd
     if (!is.null(values))             vl$x$encoding[[chnl]]$axis$values <- values
+
+    #Title
     if (!is.null(title))              vl$x$encoding[[chnl]]$axis$title <- title
-    if (!is.null(titleColor))         vl$x$encoding[[chnl]]$axis$titleColor <- titleColor
-    if (!is.null(titleFont))          vl$x$encoding[[chnl]]$axis$titleFont <- titleFont
-    if (!is.null(titleFontWeight))    vl$x$encoding[[chnl]]$axis$titleFontWeight <- titleFontWeight
-    if (!is.null(titleFontSize))      vl$x$encoding[[chnl]]$axis$titleFontSize <- titleFontSize
-    if (!is.null(titleOffset))        vl$x$encoding[[chnl]]$axis$titleOffset <- titleOffset
     if (!is.null(titleMaxLength))     vl$x$encoding[[chnl]]$axis$titleMaxLength <- titleMaxLength
-    if (!is.null(characterWidth))     vl$x$encoding[[chnl]]$axis$characterWidth <- characterWidth
-    if (!is.null(orient))             vl$x$encoding[[chnl]]$axis$orient <- orient
-    if (!is.null(format))             vl$x$encoding[[chnl]]$axis$format <- format
+    if (!is.null(titlePadding))     vl$x$encoding[[chnl]]$axis$titlePadding <- titlePadding
+
   }
 
   vl
@@ -149,7 +134,7 @@ axis_y <- function(vl, ...) {
 #'   mark_bar()
 
 axis_facet_col <- function(vl, ...) {
-  vl <- axis_vl(vl, chnl = "column", ...)
+  warning("axis_facet_col not supported by vegalite 2")
   vl
 }
 
@@ -157,6 +142,6 @@ axis_facet_col <- function(vl, ...) {
 #' @rdname axis_vl
 #' @export
 axis_facet_row <- function(vl, ...) {
-  vl <- axis_vl(vl, chnl = "row", ...)
+  warning("axis_facet_row not supported by vegalite 2")
   vl
 }
